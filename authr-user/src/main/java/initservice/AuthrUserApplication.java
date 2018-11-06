377581ddf5602e0c4e1efff3e66b9bfc94b3123c
1428486149@qq.com <1428486149@qq.com>
2018-11-06 19:57:25 +0800

    登陆服务创建

---
+++ authr-user/src/main/java/initservice/AuthrUserApplication.java
@@ -0,0 +1,14 @@
+package initservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AuthrUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthrUserApplication.class, args);
+	}
+}
