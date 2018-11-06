4f024682c3964f173271d2f00be45324558731a1
1428486149@qq.com <1428486149@qq.com>
2018-11-06 20:13:54 +0800

    合并

---
+++ initserver-zuul/src/main/java/com/ckia/initservice/InitserverZuulApplication.java
@@ -0,0 +1,12 @@
+package com.ckia.initservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitserverZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitserverZuulApplication.class, args);
+	}
+}
